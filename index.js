4ba558cb4963ab9701f318ef1bfd45af0a6f785b
ajroudirida <hpwin10kamilio@gmail.com>
2023-07-03 23:27:05 +0100

    fix linter errors and add remove function

--- index.js
+++ index.js
@@ -1,58 +1,69 @@
 let books = [];
-let localStorage = window['localStorage'];
+const { localStorage } = window;
+
+const display = () => {
+  const bookList = document.querySelector('.list-book-body');
+  bookList.innerHTML = '';
+  const data = localStorage.getItem('books');
+  if (data) {
+    books = JSON.parse(data);
+    // console.log(JSON.parse(localStorage.getItem("books")));
+  } else {
+    books = [];
+  }
+  for (let i = 0; i < books.length; i += 1) {
+    const bookItem = `
+      <div class="book-details">
+        <p class="title">${books[i].title}</p>
+        <p class="author">${books[i].author}</p>
+      </div>
+      <button onclick="removeBook(event)">Remove</button>
+`;
+
+    const bookContainer = document.createElement('div');
+    bookContainer.setAttribute('class', 'book');
+    bookContainer.innerHTML = bookItem;
+    bookList.appendChild(bookContainer);
+  }
+};
+
 const addBook = (book) => {
   books.push(book);
-  localStorage.setItem("books", JSON.stringify(books));
-  
-}
+  localStorage.setItem('books', JSON.stringify(books));
+  display();
+};
 
+/* eslint-disable no-unused-vars */
 const removeBook = (event) => {
-  // books = books.filter(item => item!== book)
+  const btn = event.target;
+  const title = btn.parentElement.getElementsByClassName('title')[0].innerText;
+  const author = btn.parentElement.getElementsByClassName('author')[0].innerText;
+  const obj = {
+    title,
+    author,
+  };
 
-  let btn = event.target;
-  let title = btn.parentElement.getElementsByClassName("title")[0].innerText;
-  let author = btn.parentElement.getElementsByClassName("author")[0].innerText;
-  let obj = {
-    title: title,
-    author: author
-  }
+  const newBooks = books.filter((item) => item.title !== obj.title && item.author !== obj.author);
+  localStorage.setItem('books', JSON.stringify(newBooks));
+  display();
+};
 
-  // books = books.filter(item => item!== obj);
-}
-
-let form = document.querySelector("form");
-form.addEventListener("submit", (e) => {
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
   e.preventDefault();
-  let title = form.elements['title'].value;
-  let author = form.elements['author'].value;
-  let book = {
-    title: title,
-    author: author
-  }
+  const title = form.elements.title.value;
+  const author = form.elements.author.value;
+  const book = {
+    title,
+    author,
+  };
   addBook(book);
 
-    let target = document.querySelector(".list-book h2");
-
-
-      let book_item = `
-        <div class="book-details">
-          <p class="title">${books[books.length-1].title}</p>
-          <p class="author">${books[books.length-1].author}</p>
-        </div>
-        <button onclick="removeBook(event)">Remove</button>
-  `;
-
-    let bookContainer = document.createElement("div");
-    bookContainer.setAttribute("class", "book");
-    bookContainer.innerHTML = book_item;
-    target.insertAdjacentElement("afterend", bookContainer);
   form.reset();
 });
 
-
-
-window.addEventListener("load", () => {
-  const data = localStorage.getItem("books");
+window.addEventListener('load', () => {
+  const data = localStorage.getItem('books');
   if (data) {
     books = JSON.parse(data);
     // console.log(JSON.parse(localStorage.getItem("books")));
@@ -61,25 +72,4 @@ window.addEventListener("load", () => {
   }
 
   display();
-  console.log(books);
 });
-
-
-const display = () => {
-  for (let i = 0; i < books.length; i += 1) {
-    let book_item = `
-      <div class="book-details">
-        <p class="title">${books[i].title}</p>
-        <p class="author">${books[i].author}</p>
-      </div>
-      <button onclick="removeBook(event)">Remove</button>
-`;
-
-    let target = document.querySelector(".list-book h2");
-    let bookContainer = document.createElement("div");
-    bookContainer.setAttribute("class", "book");
-    bookContainer.innerHTML = book_item;
-    target.insertAdjacentElement("afterend", bookContainer);
-    console.log(books);
-  }
-}
